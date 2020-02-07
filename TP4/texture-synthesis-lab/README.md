79bd2eee7e80a0cf7c44e9d810016b8829eb8756
LaurenRolan <lauren.rolan@ecole.ensicaen.fr>
2020-02-07 11:41:48 +0100

    Finished exercise3

---
+++ TP4/texture-synthesis-lab/README.md
@@ -0,0 +1,26 @@
+See slides at 
+https://docs.google.com/presentation/d/1P9MH_pYsJnmArSpUqTD-EnNtstcDyD-MH1mZkhcQ9oo/edit?usp=sharing
+
+
+For exercise 1, include the code
+For exercises 2.1-2.5, include the code and any images you find relevant
+For 2.3-2.5 include a small comment as well explaining your results.
+
+Zip results and email me at:
+ryan.webster@unicaen.fr
+
+Finally, answer the following questions:
+
+2.6: 
+Why does y_feats have the same dimension regardless of what scale we use?
+
+2.7:
+Synthesize an image with different numbers of scales, what is the effect of having more/ less scales?
+
+
+References:
+Texture Synthesis with Convolutional Neural Networks:
+https://arxiv.org/abs/1505.07376
+
+Texture Networks:
+https://arxiv.org/abs/1603.03417
---
+++ TP4/texture-synthesis-lab/exercise_1.py
@@ -0,0 +1,46 @@
+import torch
+# EXERCISE 1.1: Implement ReLU forward and backward 
+# ReLU is defined in slides
+def my_ReLU(x):
+    # Hint: x<=0 is a logical array, with 1 where x>0, and 0 where x<0
+    # answer here
+    return torch.mul(x, (x>=0).float())
+
+
+# EXERCISE 1.2: Backward of ReLU
+def ReLU_backward(x,dzdy):
+    # Hint: dzdy is multiplied by the derivative of ReLU(x), with is 1 when w>0 and 0 when x<0
+    # answer here
+    return torch.mul(dzdy, (x>=0).float())
+    
+# EXERCISE 1.3: Implement backward of matrix multiplication w.r.t. x
+def mm_backward(A,x,dzdy):
+    # answer here
+    Ax = A.mm(x)
+    return ReLU_backward(Ax,dz)
+
+
+# Exercise 1.4: Compute derivative of function composition ReLU(Ax) w.r.t. x
+x = torch.randn(5,1)
+A = torch.randn(5,5)
+
+with torch.no_grad():
+    y = A.mm(x)
+    z = my_ReLU(y)
+    dz = torch.randn(5,1)
+    dzdy = ReLU_backward(y,dz)
+    my_dzdx = mm_backward(A, x, dzdy)
+
+
+# Import actual relu function, which has a derivative implemented in pytorch
+import torch.nn as nn
+relu = nn.ReLU()
+x.requires_grad = True
+y = A.mm(x)
+z = relu(y)
+z.backward(dz)
+
+print(x.grad)
+print(my_dzdx)
+
+print('These two vectors should be the same!')
